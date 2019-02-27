8a668014d8272277e7891b767c26ef654825b7df
AneeqS <aneeqshah27@gmail.com>
2019-02-27 15:26:03 -0500

    added express

---
+++ server.js
@@ -0,0 +1,18 @@
+/**
+ * Created by AneeqShah on 2/27/19.
+ */
+
+const express = require('express');
+const app = express();
+
+app.use(express.static('build'));
+
+app.get('*', (req, res) => {
+   res.sendfile(`${__dirname}/build/index.html`);
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () =>{
+   console.log("Server started on port: ", port);
+});
